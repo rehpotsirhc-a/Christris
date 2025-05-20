ecc8f280ab0d4577d16cd5bdd75d06630322788b
Slugger <christopher.martinez24a@student.cart.org>
2025-05-20 09:28:07 -0700

    JavaScript started
    Set ceiling and floor math. Ready to create the tetriminos and their randomized order.

---
+++ script.js
@@ -0,0 +1,16 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateSequence() {
+    const sequence = ['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
+
+    while (sequence.length) {
+        const rand = getRandomInt(0, sequence.length - 1);
+        const name = sequence.splice(rand, 1)[0];
+        tetrominoSequence.push(name);
+    }
+}
