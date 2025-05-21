aa4a0564cf62ee11532e95818af23f632afca67b
Slugger <christopher.martinez24a@student.cart.org>
2025-05-21 09:51:16 -0700

    Game logic
    Added sequence math, added tetrimino appearance math, tetrimino manipulation logic, line logic.

--- script.js
+++ script.js
@@ -1,16 +1,118 @@
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
+const grid = 32;
+const tetrominoSequence = [];
+const playfield = [];
+let paused = false;
 
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+const colors = {
+    'I': 'Cyan',
+    'O': 'Gold',
+    'T': 'BlueViolet',
+    'S': 'LawnGreen',
+    'Z': 'Red',
+    'J': 'DodgerBlue',
+    'L': 'Orange',
+};
+
+for (let row = -2; row < 20; row++) {
+  playfield[row] = [];
+  for (let col = 0; col < 10; col++) playfield[row][col] = 0;
+}
+
+const tetrominos = {
+  'I': [[0,0,0,0],[1,1,1,1],[0,0,0,0],[0,0,0,0]],
+  'J': [[1,0,0],[1,1,1],[0,0,0]],
+  'L': [[0,0,1],[1,1,1],[0,0,0]],
+  'O': [[1,1],[1,1]],
+  'S': [[0,1,1],[1,1,0],[0,0,0]],
+  'Z': [[1,1,0],[0,1,1],[0,0,0]],
+  'T': [[0,1,0],[1,1,1],[0,0,0]],
+};
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function generateSequence() {
-    const sequence = ['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
-
-    while (sequence.length) {
-        const rand = getRandomInt(0, sequence.length - 1);
-        const name = sequence.splice(rand, 1)[0];
-        tetrominoSequence.push(name);
-    }
+  const sequence = ['I','J','L','O','S','T','Z'];
+  while (sequence.length) {
+    const rand = getRandomInt(0, sequence.length - 1);
+    const name = sequence.splice(rand, 1)[0];
+    tetrominoSequence.push(name);
+  }
 }
+
+function getNextTetromino() {
+  if (tetrominoSequence.length === 0) generateSequence();
+  const name = tetrominoSequence.pop();
+  const matrix = tetrominos[name];
+  const col = playfield[0].length / 2 - Math.ceil(matrix[0].length / 2);
+  const row = name === 'I' ? -1 : -2;
+  return { name, matrix, row, col };
+}
+
+function rotate(matrix) {
+  const N = matrix.length - 1;
+  return matrix.map((row, i) =>
+    row.map((_, j) => matrix[N - j][i])
+  );
+}
+
+function isValidMove(matrix, cellRow, cellCol) {
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (
+        matrix[row][col] &&
+        (cellCol + col < 0 ||
+         cellCol + col >= playfield[0].length ||
+         cellRow + row >= playfield.length ||
+         playfield[cellRow + row][cellCol + col])
+      ) return false;
+    }
+  }
+  return true;
+}
+
+function placeTetromino() {
+  for (let row = 0; row < tetromino.matrix.length; row++) {
+    for (let col = 0; col < tetromino.matrix[row].length; col++) {
+      if (tetromino.matrix[row][col]) {
+        if (tetromino.row + row < 0) return showGameOver();
+        playfield[tetromino.row + row][tetromino.col + col] = tetromino.name;
+      }
+    }
+  }
+
+  let linesCleared = 0;
+  for (let row = playfield.length - 1; row >= 0;) {
+    if (playfield[row].every(cell => !!cell)) {
+      linesCleared++;
+      for (let r = row; r >= 0; r--) {
+        for (let c = 0; c < playfield[r].length; c++) {
+          playfield[r][c] = playfield[r-1][c];
+        }
+      }
+    } else row--;
+  }
+
+  if (linesCleared > 0) {
+    lineCount += linesCleared;
+    combo++;
+    score += linesCleared * 100 + combo * 50;
+  } else {
+    combo = 0;
+  }
+
+  updateInfo();
+  tetromino = heldThisTurn ? getNextTetromino() : nextTetromino;
+  nextTetromino = getNextTetromino();
+  heldThisTurn = false;
+  updatePreview();
+}
+
+
+
+
+
+
